98acc851c5115d040617916055d991bdf3cb4dfe
jachii <jettymcfiggs@gmail.com>
2023-08-26 21:56:47 -0400

    Added tests for volumes

--- src/volumes.rs
+++ src/volumes.rs
@@ -1,24 +1,113 @@
+use serde::Deserialize;
 use std::collections::HashMap;
 
+#[derive(Debug, Deserialize)]
 pub struct Volumes {
-    volumes: HashMap<String, VolumeAttributes>,
+    volumes: HashMap<String, Option<VolumeAttributes>>,
 }
 
+#[derive(Debug, Deserialize)]
 pub struct VolumeAttributes {
-    driver: String, // enum
-    driver_opts: DriverOpts,
-    external: String,
-    labels: Vec<String>, // could be hashmap string,string
-    name: String,
+    driver: Option<String>,
+    driver_opts: Option<DriverOpts>,
+    external: Option<String>,
+    labels: Option<Vec<String>>, // could be hashmap string,string
+    name: Option<String>,
 }
 
+#[derive(Debug, Deserialize)]
 pub struct DriverOpts {
-    driver_type: String, // rename to type
-    o: String,
-    device: String,
+    #[serde(rename = "type")]
+    driver_type: Option<String>,
+    o: Option<String>,
+    device: Option<String>,
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde_yaml;
+
+    #[test]
+    fn test_deserialize_correct_volume() {
+        let yaml = r#"
+        volumes:
+            db-data:
+                external: true
+        "#;
+        let volumes: Volumes = serde_yaml::from_str(yaml).unwrap();
+        assert_eq!(volumes.volumes.len(), 1);
+        assert!(volumes.volumes.get("db-data").is_some());
+    }
+
+    #[test]
+    fn test_deserialize_multiple_volumes() {
+        let yaml = r#"
+            volumes:
+              "vol1":
+                driver: "driver1"
+              "vol2":
+                driver: "driver2"
+                driver_opts:
+                  type: "type1"
+                  o: "option1"
+              "vol3":
+                external: "ext_vol"
+                labels:
+                  - "label1"
+                  - "label2"
+              "vol4":
+                name: "named_vol"
+        "#;
+
+        let volumes: Volumes = serde_yaml::from_str(yaml).unwrap();
+
+        assert_eq!(volumes.volumes.len(), 4);
+        assert_eq!(
+            volumes.volumes["vol1"].as_ref().unwrap().driver,
+            Some("driver1".to_string())
+        );
+        assert_eq!(
+            volumes.volumes["vol2"].as_ref().unwrap().driver,
+            Some("driver2".to_string())
+        );
+        assert_eq!(
+            volumes.volumes["vol2"]
+                .as_ref()
+                .unwrap()
+                .driver_opts
+                .as_ref()
+                .unwrap()
+                .driver_type,
+            Some("type1".to_string())
+        );
+        assert_eq!(
+            volumes.volumes["vol2"]
+                .as_ref()
+                .unwrap()
+                .driver_opts
+                .as_ref()
+                .unwrap()
+                .o,
+            Some("option1".to_string())
+        );
+        assert_eq!(
+            volumes.volumes["vol3"].as_ref().unwrap().external,
+            Some("ext_vol".to_string())
+        );
+        assert_eq!(
+            volumes.volumes["vol3"]
+                .as_ref()
+                .unwrap()
+                .labels
+                .as_ref()
+                .unwrap()
+                .len(),
+            2
+        );
+        assert_eq!(
+            volumes.volumes["vol4"].as_ref().unwrap().name,
+            Some("named_vol".to_string())
+        );
+    }
 }
