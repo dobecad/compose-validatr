2a578898a690c063922c24d55b6b4f393df40149
jachii <jettymcfiggs@gmail.com>
2023-08-26 21:15:57 -0400

    Secrets

---
+++ src/secrets.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+
+pub struct Secrets {
+    secrets: HashMap<String, SecretAttributes>
+}
+
+pub struct SecretAttributes {
+    file: String,
+    environment: String,
+    external: bool,
+    name: String,
+}
