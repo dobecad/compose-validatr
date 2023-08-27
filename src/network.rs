a9d0acd72984d0ff4dc4d8a09faa9ec417dd3c0e
jachii <jettymcfiggs@gmail.com>
2023-08-26 21:09:48 -0400

    Network

---
+++ src/network.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+pub struct Network {
+    network: HashMap<String, NetworkAttributes>, // String, network attributes
+}
+
+pub struct NetworkAttributes {
+    ipam: String,   // struct
+    driver: String, // enum
+    config: Vec<Config>,
+    internal: bool,
+    labels: Vec<String>, // or hashmap string,string
+    name: String,
+}
+
+pub struct Config {
+    subnet: String,
+    ip_range: String,
+    gateway: String,
+    aux_addresses: AuxAddresses,
+}
+
+pub struct AuxAddresses {
+    addresses: HashMap<String, String>,
+}
