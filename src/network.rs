2cb275b6d5615efea8bfde1ff9a33054c33fc086
jachii <jettymcfiggs@gmail.com>
2023-08-28 12:53:50 -0400

    Network

--- src/network.rs
+++ src/network.rs
@@ -3,25 +3,50 @@ use std::collections::HashMap;
 
 #[derive(Debug, Deserialize)]
 pub struct Network {
-    network: HashMap<String, Option<NetworkAttributes>>, // String, network attributes
+    network: Option<HashMap<String, Option<NetworkAttributes>>>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct NetworkAttributes {
-    pub ipam: String,   // struct
-    pub driver: String, // enum
-    pub config: Vec<Config>,
-    pub internal: bool,
-    pub labels: Vec<String>, // or hashmap string,string
-    pub name: String,
+    pub attachable: Option<bool>,
+    pub ipam: Option<Ipam>,
+    pub driver: Option<Driver>,
+    pub driver_opts: Option<HashMap<String, String>>,
+    pub enable_ipv6: Option<bool>,
+    pub external: Option<bool>,
+    pub config: Option<Vec<Config>>,
+    pub internal: Option<bool>,
+    pub labels: Option<Labels>,
+    pub name: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Ipam {
+    pub driver: Option<Driver>,
+    pub config: Option<Config>,
+    pub options: Option<HashMap<String, String>>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Driver {
+    None,
+    Host,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+pub enum Labels {
+    List(Vec<String>),
+    Map(HashMap<String, String>),
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
-    pub subnet: String,
-    pub ip_range: String,
-    pub gateway: String,
-    pub aux_addresses: AuxAddresses,
+    pub subnet: Option<String>,
+    pub ip_range: Option<String>,
+    pub gateway: Option<String>,
+    pub aux_addresses: Option<AuxAddresses>,
 }
 
 #[derive(Debug, Deserialize)]
