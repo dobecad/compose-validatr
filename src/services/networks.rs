31ebd12d27fe93209f556cb917018de2cd8fe1da
jachii <jettymcfiggs@gmail.com>
2023-11-17 19:14:05 -0500

    Added tests

--- src/services/networks.rs
+++ src/services/networks.rs
@@ -30,7 +30,67 @@ pub struct NetworkOptions {
 }
 
 impl Validate for Networks {
-    fn validate(&self, _: &Compose, _: &mut crate::errors::ValidationErrors) {
-        ()
+    fn validate(&self, ctx: &Compose, errors: &mut crate::errors::ValidationErrors) {
+        match self {
+            Networks::List(n) => {
+                ctx.networks.as_ref().map(|available_networks| {
+                    n.iter().for_each(|network| {
+                        if !available_networks.contains_key(network) {
+                            errors.add_error(crate::errors::ValidationError::InvalidValue(
+                                format!("Service networks reference an unknown network: {network}"),
+                            ));
+                        }
+                    });
+                });
+            }
+            Networks::Map(n) => {
+                ctx.networks.as_ref().map(|available_networks| {
+                    n.keys().for_each(|network| {
+                        if !available_networks.contains_key(network) {
+                            errors.add_error(crate::errors::ValidationError::InvalidValue(
+                                format!("Service networks reference an unknown network: {network}"),
+                            ));
+                        }
+                    });
+                });
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn invalid_network() {
+        let yaml = r#"
+        services:
+          some-service:
+            networks:
+              - hello
+              - world
+        networks:
+            some-network:
+            other-network:
+        "#;
+        let compose = Compose::new(yaml);
+        assert!(compose.is_err());
+    }
+
+    #[test]
+    fn valid_network() {
+        let yaml = r#"
+        services:
+          some-service:
+            networks:
+              - some-network
+              - other-network
+        networks:
+            some-network:
+            other-network:
+        "#;
+        let compose = Compose::new(yaml);
+        assert!(compose.is_ok());
     }
 }
